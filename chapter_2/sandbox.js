af51ffc7822c97785e5e1c1d7dd23a13aac952fa
montezuma00 <dduval2@rrc.ca>
2024-08-29 13:35:29 -0500

    comparison

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,24 +1,14 @@
-let ninjas = ['shaun', 'ryu', 'chun-li'];
+let age = 25;
 
-// ninjas[1] = 'ken';
+// loose comparison (different types can still be equal)
 
-// console.log(ninjas[1]);
+// console.log(age == 25);
+// console.log(age == '25'); // still true, even though different types
+// console.log(age != 25);
+// console.log(age != '25');
 
-// let ages = [20,25,30,35];
-// console.log(ages[2]);
-
-let random = ['shaun', 'crystal', 30, 20];
-// console.log(random);
-
-// console.log(ninjas.length);
-
-// let result = ninjas.join(', ');
-
-// let result = ninjas.indexOf('chun-li');
-// let villains = ['bison', 'vega', 'balrog'];
-// let result = ninjas.concat(villains);
-// let result = ninjas.concat(['ken', 'crystal']);
-let result = ninjas.push('ken');
-result = ninjas.pop();
-
-console.log(ninjas);
+// strict comparison (different types cannot be equal) 
+console.log(age === 25);
+console.log(age === '25'); // false! java is checking type now, too
+console.log(age !== 25);
+console.log(age !== '25');
