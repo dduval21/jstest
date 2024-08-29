3d260cd7d215dd94caa742b7272c3b18178851c0
montezuma00 <dduval2@rrc.ca>
2024-08-29 15:22:57 -0500

    else if statements

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,19 +1,11 @@
-// if statements
+// else if statements
 
-// const age = 25;
+const password = 'p@ss';
 
-// if (age > 20){
-//     console.log('you are over 20 years old');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if(ninjas.length > 3){
-//     console.log("that's a lot of ninjas");
-// }
-
-const password = 'password';
-
-if(password.length >= 8){
+if(password.length >= 12){
+    console.log('that password is mighty strong');
+} else if(password.length >= 8){
     console.log('that password is long enough');
+} else {
+    console.log('password is not long enough');
 }
