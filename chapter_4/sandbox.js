8c77308227d8a91e0c6102903b846a0db9807204
montezuma00 <dduval2@rrc.ca>
2024-08-30 11:16:47 -0500

    regular and arrow functions + practice

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,24 +1,48 @@
-// returning values
+// regular function
 
-const calcArea = function(radius){
-    let area = 3.14 * radius**2;
-    return area;
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
+
+// arrow function (a little more simple to write)
+const calcArea = (radius) => {
+    return 3.14 * radius**2;
 };
 
-// since we're returning area, now we can save that value in memory 
-// when the function is called.
-const areaResult = calcArea(5);
-console.log(areaResult); // 78.5
+// you can also remove the parentheses when there is just one parameter
+// const calcArea = radius => {
+//     return 3.14 * radius**2;
+// };
 
-// trying example on my own
-const calcVol = function(area, height){
-    let vol = area * height;
-    return vol;
-}
+// if one return, then we can omit the return keyword as well
+// const calcArea = radius => {3.14 * radius**2};
 
-const areaGiven = 10;
-const heightGiven = 2;
-console.log(calcVol(areaGiven, heightGiven));
-// or
-let result = calcVol(areaGiven, heightGiven);
+const area = calcArea(5);
+console.log('area is:', area);
+
+// arrow function practice - turn these into arrow functions
+// const greet = function(){
+//     return 'hello world';
+// }
+
+const greet = () => 'hello world';
+const result = greet();
 console.log(result);
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+};
+
+console.log( bill([10, 15, 30], 0.2) );
