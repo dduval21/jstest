80a47b8a2fc41d64d117c231c54906c6db5f4271
montezuma00 <dduval2@rrc.ca>
2024-08-30 11:45:41 -0500

    callback functions

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,14 +1,34 @@
-const name = 'shaun';
+// callbacks & foreach
+// you can pass a function as an argument
 
-// functions
+// const myFunc = (callbackFunc) => {
+//     // do something
+//     let value = 50;
+//     callbackFunc(value);
+// }
 
-const greet = () => 'hello';
+// myFunc(function(value){
+//     // do something
+//     console.log(value);
 
-let resultOne = greet();
-console.log(resultOne);
+// });
 
-// methods
 
-let resultTwo = name.toUpperCase();
-console.log(resultTwo);
 
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+// also: people.forEach(function(person){})
+// people.forEach(person => {
+//     console.log(person);
+// });
+
+
+// people.forEach((person,index) => {
+//     console.log(person);
+// });
+
+const logPerson = (person, index) => {
+    console.log(`${index} - hello ${person} `)
+}
+
+people.forEach(logPerson);
