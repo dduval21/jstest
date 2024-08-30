969f091f372c00b17a6bece5b917f3190035bccb
montezuma00 <dduval2@rrc.ca>
2024-08-30 13:54:06 -0500

    storing objects in arrays

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,51 +1,35 @@
 // object literals
 
+// const blogs = [
+//     { title: 'why mac & cheese rules', likes: 30},
+//     { title: '10 things to make with marmite', likes: 50}
+// ];
+
+// console.log(blogs);
+
 let user = {
     name: 'crystal',
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    blogs: [
+        { title: 'why mac & cheese rules', likes: 30},
+        { title: '10 things to make with marmite', likes: 50}
+    ],
     login: function(){
         console.log('the user logged in');
     },
     logout: function(){
         console.log('the user logged out');
     },
-    logBlogs: function(){
-        // console.log(this);
+    logBlogs(){
+        // console.log(this.blogs);
         console.log('This user has written the following blogs: ');
         this.blogs.forEach(blog => {
-            console.log(blog);
+            console.log(blog.title, blog.likes);
         });
     }
 };
 
-// when using 'this' keyword, JS sets the value of the 'this' keyword to
-// the object that the method was used on.
-
 user.logBlogs();
-console.log(this);
-
-// NOTE: JS will not do that for an arrow function. 
-// It takes what it currently is and passes it into the function
-// e.g. logBlogs: () => {
-    // }
-    // console.log(this)   // will just display the window
-
-// an easy way to use regular functions is to write it as such:
-//logblogs(){}
-
-// will work with this
-
-// console.log(user);
-// console.log(user.name);
-
-// user.age = 35;
-// console.log(user.age);
-
-// console.log(user['name']);
-// user['name'] = 'chun-li'; // another way to update values
-// console.log(user['name']);
-
 
