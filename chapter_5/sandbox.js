35a5e8748dae89b92dce293843ff38f3c3607441
montezuma00 <dduval2@rrc.ca>
2024-08-30 14:30:04 -0500

    reference vs primitive types, end of Ch 5

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,18 +1,33 @@
-// Math object
+// primitive types: numbers, strings, bools, null, undefined, symbols
+// reference types: all objects; literals, arrays, functions, dates, etc
+// primitives are stored on the stack, reference types (objects) are on heap
+// the stack points to objects in the heap
 
-console.log(Math.PI);
+// if you create an object that is equal to another object, 
+// you will not make a new object. You will make a new POINTER
+// and both will point to the same object in the heap
 
-console.log(Math.E);
+// primitive values
 
-const area = 7.7;
+// let scoreOne = 50;
+// let scoreTwo = scoreOne;
 
-console.log(Math.round(area)); //floor(area) will round to 7, ceil is opposite that
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
 
-console.log(Math.trunc(area));
+// scoreOne = 100;
 
-// random numbers
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);  // 100, 50
+// // when we make a copy of primitives, they do not change when one updates
 
-const random = Math.random(); // always between 0 and 1
+// reference values
 
-console.log(random);
-console.log((random)*100);
+const userOne = { name: 'ryu', age: 30}
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.age = 40;
+
+console.log(userOne, userTwo); // both age 40
+// reference value changed for both since the object in the heap was updated
+// reference types are stored ONCE
