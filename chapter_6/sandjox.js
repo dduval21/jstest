69d02b3ab5941f91d1d5973f4fee6c87d4145313
montezuma00 <dduval2@rrc.ca>
2024-08-30 15:40:07 -0500

    quick commit before DOM challenge (46:48)

--- chapter_6/sandjox.js
+++ chapter_6/sandjox.js
@@ -1,19 +1,10 @@
-const title = document.querySelector('h1');
+const content = document.querySelector('p');
 
-title.setAttribute('style', 'margin: 50px');
-// this will overwrite the style completely
-// how do we only modify/add one style
+console.log(content.classList);
+// to get classes that an element has
 
-console.log(title.style);
-console.log(title.style.color);
+content.classList.add('error');
+// adding a class to it
 
-title.style.margin = '50px';
-title.style.color = 'crimson';
-
-title.style.fontSize = '60px'; 
-// the style in css is font-size, but this will read weird in JS
-// fontSize is the alternate for JS
-
-title.style.margin = ''; // removes that property by doing this
-
-
+content.classList.remove('error');
+content.classList.add('success');
