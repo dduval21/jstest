9e1fc76e620ee150ef7d362dd58a8d48c028e556
montezuma00 <dduval2@rrc.ca>
2024-08-30 10:30:14 -0500

    practice with parameters and passing arguments

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,22 +1,26 @@
-// // function declaration - is always hoisted to top of file
-// function greet(){
-//     console.log('hello there');
-// }
+// arguments & parameters
 
-// function expression - is not hoisted to top of file
-const speak = function(){
-    console.log('good day');
-};    // semicolon at end
+// when we make the function, we define a parameter to be used in the function
+const speak = function(name){   
+    console.log(`good day ${name}`);
+};
 
-greet();
-greet();
-greet();
+// console.log(name) --> doesn't work because name is local variable
 
-speak();
-speak();
-speak();
+// now we pass a value 'mario' (an argument) when we invoke the function:
+// result: 'good day mario'
+speak('mario');
 
-// function declaration - is always hoisted to top of file
-function greet(){
-    console.log('hello there');
-}
+// 2 parameters
+const speakAgain = function(name, time){   
+    console.log(`good ${time} ${name}`);
+};
+
+speakAgain('mario', 'morning'); // good morning mario
+
+// invoking without arguments - need default values
+const speakMore = function(name = 'gamer', time = 'evening'){   
+    console.log(`good ${time} ${name}`);
+};
+
+speakMore(); // good evening gamer
