8512d63bf4721652a6180c5aaec0fe496826c639
montezuma00 <dduval2@rrc.ca>
2024-08-30 15:24:56 -0500

    changing individual styles

--- chapter_6/sandjox.js
+++ chapter_6/sandjox.js
@@ -1,22 +1,19 @@
-const link = document.querySelector('a');
+const title = document.querySelector('h1');
 
-console.log(link.getAttribute('href'));
-// google.com, takes the attribute of the 1st a tag
+title.setAttribute('style', 'margin: 50px');
+// this will overwrite the style completely
+// how do we only modify/add one style
+
+console.log(title.style);
+console.log(title.style.color);
+
+title.style.margin = '50px';
+title.style.color = 'crimson';
+
+title.style.fontSize = '60px'; 
+// the style in css is font-size, but this will read weird in JS
+// fontSize is the alternate for JS
+
+title.style.margin = ''; // removes that property by doing this
 
 
-link.setAttribute('href', 'https://thenetninja.co.uk');
-// now links to net ninja site
-link.innerText = 'TheNet Ninja Website';
-// changes the text of the link
-
-const mssg = document.querySelector('p');
-
-console.log(mssg.getAttribute('class'));
-// gets 'error' i.e. the class of that p tag
-
-mssg.setAttribute('class', 'success');
-// changes the class from error to success
-
-mssg.setAttribute('style', 'color:green');
-// there was no style attribute, but JS added it on anyway
-// text is now green
