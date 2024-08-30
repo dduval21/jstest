9bddee02c71e15f236453d070f8d50189a457725
montezuma00 <dduval2@rrc.ca>
2024-08-30 11:20:21 -0500

    quick exercise on methods

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,48 +1,14 @@
-// regular function
+const name = 'shaun';
 
-// const calcArea = function(radius){
-//     return 3.14 * radius**2;
-// }
+// functions
 
-// arrow function (a little more simple to write)
-const calcArea = (radius) => {
-    return 3.14 * radius**2;
-};
+const greet = () => 'hello';
 
-// you can also remove the parentheses when there is just one parameter
-// const calcArea = radius => {
-//     return 3.14 * radius**2;
-// };
+let resultOne = greet();
+console.log(resultOne);
 
-// if one return, then we can omit the return keyword as well
-// const calcArea = radius => {3.14 * radius**2};
+// methods
 
-const area = calcArea(5);
-console.log('area is:', area);
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
 
-// arrow function practice - turn these into arrow functions
-// const greet = function(){
-//     return 'hello world';
-// }
-
-const greet = () => 'hello world';
-const result = greet();
-console.log(result);
-
-// const bill = function(products, tax){
-//     let total = 0;
-//     for(let i = 0; i < products.length; i++){
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total;
-// }
-
-const bill = (products, tax) => {
-    let total = 0;
-    for(let i = 0; i < products.length; i++){
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-};
-
-console.log( bill([10, 15, 30], 0.2) );
